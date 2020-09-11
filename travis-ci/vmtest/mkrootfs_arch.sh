9c1736a21fc50bd2fecbb00f2993d1f5189884ce
Ilya Leoshkevich <iii@linux.ibm.com>
2020-09-11 21:27:59 +0200

    vmtests: add debootstrap-based mkrootfs script
    The existing mkrootfs.sh is based on Arch Linux, which supports only
    x86_64.
    
    Add mkrootfs_debian.sh: a debootstrap-based script. Debian was chosen,
    because it supports more architectures than other mainstream distros.
    Move init setup to mkrootfs_tweak.sh, rename the existing Arch script
    to mkrootfs_arch.sh.
    
    Add mount /proc to S10-mount: for some reason mount -a from Debian
    busybox does nothing if /proc is not mounted.
    
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>

--- travis-ci/vmtest/mkrootfs_arch.sh
+++ travis-ci/vmtest/mkrootfs_arch.sh
@@ -99,50 +99,7 @@ rm -rf "$root/var/lib/pacman/sync/"
 # We don't need any documentation.
 rm -rf "$root/usr/share/{doc,help,man,texinfo}"
 
-chroot "${root}" /bin/busybox --install
+"$(dirname "$0")"/mkrootfs_tweak.sh "$root"
 
-cat > "$root/etc/fstab" << "EOF"
-dev /dev devtmpfs rw,nosuid 0 0
-proc /proc proc rw,nosuid,nodev,noexec 0 0
-sys /sys sysfs rw,nosuid,nodev,noexec 0 0
-debugfs /sys/kernel/debug debugfs mode=755,realtime 0 0
-bpffs /sys/fs/bpf bpf realtime 0 0
-EOF
-chmod 644 "$root/etc/fstab"
-
-cat > "$root/etc/inittab" << "EOF"
-::sysinit:/etc/init.d/rcS
-::ctrlaltdel:/sbin/reboot
-::shutdown:/sbin/swapoff -a
-::shutdown:/bin/umount -a -r
-::restart:/sbin/init
-EOF
-chmod 644 "$root/etc/inittab"
-
-mkdir -m 755 "$root/etc/init.d" "$root/etc/rcS.d"
-cat > "$root/etc/rcS.d/S10-mount" << "EOF"
-#!/bin/sh
-
-/bin/mount -a
-EOF
-chmod 755 "$root/etc/rcS.d/S10-mount"
-
-cat > "$root/etc/rcS.d/S40-network" << "EOF"
-#!/bin/sh
-
-ip link set lo up
-EOF
-chmod 755 "$root/etc/rcS.d/S40-network"
-
-cat > "$root/etc/init.d/rcS" << "EOF"
-#!/bin/sh
-
-for path in /etc/rcS.d/S*; do
-	[ -x "$path" ] && "$path"
-done
-EOF
-chmod 755 "$root/etc/init.d/rcS"
-
-chmod 755 "$root"
 tar -C "$root" -c . | zstd -T0 -19 -o "$NAME"
 chmod 644 "$NAME"
---
+++ travis-ci/vmtest/mkrootfs_tweak.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# This script prepares a mounted root filesystem for testing libbpf in a virtual
+# machine.
+set -e -u -x -o pipefail
+root=$1
+shift
+
+chroot "${root}" /bin/busybox --install
+
+cat > "$root/etc/fstab" << "EOF"
+dev /dev devtmpfs rw,nosuid 0 0
+proc /proc proc rw,nosuid,nodev,noexec 0 0
+sys /sys sysfs rw,nosuid,nodev,noexec 0 0
+debugfs /sys/kernel/debug debugfs mode=755,realtime 0 0
+bpffs /sys/fs/bpf bpf realtime 0 0
+EOF
+chmod 644 "$root/etc/fstab"
+
+cat > "$root/etc/inittab" << "EOF"
+::sysinit:/etc/init.d/rcS
+::ctrlaltdel:/sbin/reboot
+::shutdown:/sbin/swapoff -a
+::shutdown:/bin/umount -a -r
+::restart:/sbin/init
+EOF
+chmod 644 "$root/etc/inittab"
+
+mkdir -m 755 "$root/etc/init.d" "$root/etc/rcS.d"
+cat > "$root/etc/rcS.d/S10-mount" << "EOF"
+#!/bin/sh
+
+/bin/mount /proc
+/bin/mount -a
+EOF
+chmod 755 "$root/etc/rcS.d/S10-mount"
+
+cat > "$root/etc/rcS.d/S40-network" << "EOF"
+#!/bin/sh
+
+ip link set lo up
+EOF
+chmod 755 "$root/etc/rcS.d/S40-network"
+
+cat > "$root/etc/init.d/rcS" << "EOF"
+#!/bin/sh
+
+for path in /etc/rcS.d/S*; do
+	[ -x "$path" ] && "$path"
+done
+EOF
+chmod 755 "$root/etc/init.d/rcS"
+
+chmod 755 "$root"
